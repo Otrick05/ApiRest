b907166f39d6c3e3d2656d3de85425970f7ace84
Otrick05 <odritorres2@hotmail.com>
2024-09-14 20:30:38 -0700

    Primer commit

---
+++ src/main/java/com/webProjects/SushiApiApplication.java
@@ -0,0 +1,13 @@
+package com.webProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiApiApplication.class, args);
+	}
+
+}
